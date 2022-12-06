37dadbea7977fbb2a64879a0f86480f96b35039a
llama <crazyllama789@gmail.com>
2022-12-06 16:39:27 -0500

    working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <h1 class="border" style={{marginBlockEnd:'0.5em', marginBlockStart:'0.5em'}}>
+          <u>Final Portfolio</u>
+        </h1>
+        <h5> <u>Theme:</u> Finding problems within companies and fixing them </h5>
+        <div>
+        <div style={{marginTop:0}}>
+        <a style={{paddingRight:"1.5em"}} 
+        class="hover12" href="https://crazyllama789.github.io/personas_and_storyboards/">
+        <img style={{marginTop:'5em'}} src={require('./storyboard.png')}  alt="storyboard website" width="500" height="500"/>
         </a>
-      </header>
+        <ul style={{paddingLeft:'3em', float:'left', inlineSize:'45%', fontSize:'0.9em', textAlign:'left',
+      }}> <p class="border">Observation of WholeFoods Self-Checkout</p>
+        <li style={{paddingLeft: "2em", fontFamily:'Bree Serif', fontSize:'0.5em', textAlign:'left'}}>
+          <u>Context:</u> this website brings awareness to the issues revolving around the WholeFoods self-checkout machines, and I provide
+          my ideas for potential solutions on how to fix them. 
+        </li>
+        <li style={{paddingLeft: "2em", fontFamily:'Bree Serif', fontSize:'0.5em', textAlign:'left', paddingTop:"1em"}}>
+        <u>Research:</u> it allows users to learn about the interface and see how people interact with the interface through observations, personas, and storyboards
+        </li>
+        <li style={{paddingLeft: "2em", fontFamily:'Bree Serif', fontSize:'0.5em', textAlign:'left', paddingTop:"1em"}}>
+          <u>Design Iterations:</u> one of the major design decisions made here is the choice to break up each category into its own subpage. 
+          I decided to persue this design because it most resembles a real world website, and the user can quickly navigate to each aspect of the page. I also handdrew the 
+          WholeFoods logo on Procreate, so my vision for what the website would look like would be exactly what I wanted. 
+        </li>
+        <li style={{paddingLeft: "2em", fontFamily:'Bree Serif', fontSize:'0.5em', textAlign:'left', paddingTop:"1em"}}>
+        <u>Conclusion:</u> I learned the importance of creating a narrative when designing interfaces, and how important it to make the 
+          interface clear, so it is easy for users to use in the intended way. 
+        </li> 
+        </ul>
+        </div>
+        <div  style={{textAlign:'center', margin:'1em'}}>
+        <ul  style={{paddingLeft:'2em', float:'left', inlineSize:'45%', fontSize:'0.9em', textAlign:'left',
+      paddingTop:'0', marginTop:'0'}}> <p class="border">Re-Design of Don Jose Tequila's Website</p>
+        <li style={{paddingLeft: "2em", fontFamily:'Bree Serif', fontSize:'0.5em', textAlign:'left'}}>i
+        <u>Context:</u> the old Don Jose Tequilas website was confusing and challenging to navigate. It also 
+        lacked accessiblility, so I created an improved version that improved customer experience and increased clarity. 
+        </li>
+        <li style={{paddingLeft: "2em", fontFamily:'Bree Serif', fontSize:'0.5em', textAlign:'left', paddingTop:"1em"}}>
+        <u>Research:</u> the main audience of this website is costumers who wants to dine-in or dine-out with Don Jose Tequilas. 
+        </li>
+        <li style={{paddingLeft: "2em", fontFamily:'Bree Serif', fontSize:'0.5em', textAlign:'left', paddingTop:"1em"}}>
+        <u>Design Iterations:</u> the main design choices I made was making the website more aesthically pleasing, having an easier flow of the 
+          content on the website, including clear visuals to show the audience what this website is about, and having more
+          visbile font. I also drew the tequila shot logo for the website, so it would add a personal touch and help 
+          improve my vision.
+        </li>
+        <li style={{paddingLeft: "2em", fontFamily:'Bree Serif', fontSize:'0.5em', textAlign:'left', paddingTop:"1em"}}>
+        <u>Conclusion:</u> I learned the importance of creating websites that have a well-desinged hierarchy, aesthically satisfying to the users, 
+          and all the aspects are clear for how the user is meant to use it. 
+        </li>
+        </ul>
+        <a class="hover12" href="https://crazyllama789.github.io/responsive_design/homepage.html#">
+        <img style={{marginTop:'5em'}}src={require('./don_jose.png')} alt="done jose website"  width="500" height="500" />  
+        </a>
+        </div>
+        </div>
+        <div  style={{textAlign:'center', margin:'1em'}}>
+        <ul  style={{paddingLeft:'2em', float:'left', inlineSize:'45%', fontSize:'0.9em', textAlign:'left',
+      paddingTop:'0', marginTop:'0'}}> <p class="border">Re-Design of yhangry Website</p>
+        <li style={{paddingLeft: "2em", fontFamily:'Bree Serif', fontSize:'0.5em', textAlign:'left'}}>
+        <u>Context:</u> this website was designed as a way to help a startup food service company create an efficient website. I helped 
+        layout the plan for what the website should consist of and what the design should be in order to attract customer appeal.  
+        </li>
+        <li style={{paddingLeft: "2em", fontFamily:'Bree Serif', fontSize:'0.5em', textAlign:'left', paddingTop:"1em"}}>
+        <u>Research:</u> the main audience of this website is costumers who wants to hire a private chef, using the app yhangry. 
+        </li>
+        <li style={{paddingLeft: "2em", fontFamily:'Bree Serif', fontSize:'0.5em', textAlign:'left', paddingTop:"1em"}}>
+        <u>Design Iterations:</u> the main design choices I made was making the website more aesthically pleasing, having the navigation bar
+          so it would take the user to certain spots on the page, and also a clear hierarchy so aspects of the website are easy to read and understand. 
+        </li>
+        <li style={{paddingLeft: "2em", fontFamily:'Bree Serif', fontSize:'0.5em', textAlign:'left', paddingTop:"1em"}}>
+        <u>Conclusion:</u> I learned how to take an idea for a company and through what the company does create my own interpreation of what 
+          their website would look like. 
+        </li>
+        </ul>
+        <a class="hover12" href="https://exhaustedpanda557.github.io/iterativeDesign/">
+        <img style={{marginTop:'6em'}}src={require('./yhangy.png')} alt="done jose website"  width="500" height="500" />  
+        </a>
+        </div>
+
+        <div  style={{textAlign:'center', margin:'1em'}}>
+        <ul  style={{textAlign:'center', paddingLeft:'2em', float:'left', inlineSize:'45%', fontSize:'0.9em', textAlign:'left',
+      paddingTop:'0', marginTop:'0'}}> <p class="border">Zoo Animal Shopping cart</p>
+        <li style={{paddingLeft: "2em", fontFamily:'Bree Serif', fontSize:'0.5em', textAlign:'left'}}>
+        <u>Context:</u> there was no place public place that existed to purchase exotic animals for zoos. So, in order 
+        to help zookeepers I designed a website to sort and filter through specific animals and allow zookeepers to purchase them.  
+        </li>
+        <li style={{paddingLeft: "2em", fontFamily:'Bree Serif', fontSize:'0.5em', textAlign:'left', paddingTop:"1em"}}>
+        <u>Research:</u> this website makes it easy to filter animals based off of their native location and type of food they eat. 
+        It also allows users to sort by price in decreasing and increasing order. The customer can also see their total 
+        cost, as well as how much they are spending for each individual animal. 
+        </li>
+        <li style={{paddingLeft: "2em", fontFamily:'Bree Serif', fontSize:'0.5em', textAlign:'left', paddingTop:"1em"}}>
+        <u>Design Iterations:</u> the website has a clear hierarchy. The items that the customer can purchase are in the middle of the webpage, 
+          the filters on the left-side of the webpage and the cart is on the right of the webpage. The cart expands as animals 
+          are added to it. 
+        </li>
+        <li style={{paddingLeft: "2em", fontFamily:'Bree Serif', fontSize:'0.5em', textAlign:'left', paddingTop:"1em"}}>
+        <u>Conclusion:</u> I learned how to filter and sort objects based off of specific characters and also how to remove and add specific items from a cart. 
+        </li>
+        </ul>
+        <a class="hover12" href="https://crazyllama789.github.io/Development/">
+        <img style={{marginTop:'4em'}}src={require('./zoo.png')} alt="we bought a zoo website"  width="500" height="500" />  
+        </a>
+        </div>
     </div>
   );
 }
